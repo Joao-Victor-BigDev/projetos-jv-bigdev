bebc27c9ce423e8a47a8a9e837710e75938bd9f3
JV - BigDev <joaoszsz12@gmail.com>
2022-11-16 20:19:58 -0300

    atualizando codigo list check

--- Sistema de lista VIP [List check].py
+++ Sistema de lista VIP [List check].py
@@ -1,8 +1,7 @@
 print('===== Veriicador de Lista Vip =====')
 lista = ['Joao Victor', 'Thiago', 'Fernando', 'Luana']
-nome = str(input('Digite o nome para verificacao na lista vip. '))
-space = nome.strip()
-capt = space.title()
+nome = str(input('Digite o nome para verificacao na lista vip. ')).strip()
+capt = nome.title()
 if capt in lista:
     print(f'Seja bem vindo(a) a FESTA ! {nome}')
     entrou = input('O acesso esta liberado !')
